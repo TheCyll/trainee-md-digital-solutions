62b7a1a0ca9f860d85a3fa29b8b73dbe3fab24d7
Hazel <hazelwence_13@hotmail.com>
2021-05-23 03:09:45 -0500

    Added routes for pages. Added styles for content wrapper

--- src/routes/PublicRoutes/index.js
+++ src/routes/PublicRoutes/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Route } from 'react-router';
 
-import { Home } from '../../pages';
+import { Header } from '../../components';
+
+import { Home, R2, R3, R4 } from '../../pages';
+
+import './styles.css';
 
 const PublicRoutes = () => {
   return (
-    <>
-      <Route exact path='/' component={Home} /> 
-    </>
+    <div className="main-wrapper">
+      <Header />
+      <main className="content-wrapper">
+        <Route exact path='/' component={Home} /> 
+        <Route exact path='/r2' component={R2} /> 
+        <Route exact path='/r3' component={R3} /> 
+        <Route exact path='/r4' component={R4} /> 
+      </main>
+    </div>
   )
 }
 
