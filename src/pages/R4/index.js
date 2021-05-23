5a8b9067d1d8ad68ea04dd1641ee488705bb40f1
Hazel <hazelwence_13@hotmail.com>
2021-05-23 05:13:25 -0500

    Api call and table format page

--- src/pages/R4/index.js
+++ src/pages/R4/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { Table } from '../../components';
 
 const R4 = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();    
+    setPosts(data);
+    console.log(data);
+  }, []);
+
   return (
-    <div>
-      R4
+    <div className="api-table-wrapper">
+      <Table 
+        fieldsNames={["id", "Título", "Contenido"]}
+        keysToRender={["id", "title", "body"]}
+        itemsList={ posts }
+      />
     </div>
   )
 }
