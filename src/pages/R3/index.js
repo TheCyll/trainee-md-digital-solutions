5c49b90efbdba952f6324901f755c6aabe08102a
Hazel <hazelwence_13@hotmail.com>
2021-05-23 04:54:55 -0500

    Page for video modal created

--- src/pages/R3/index.js
+++ src/pages/R3/index.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import './styles.css';
 
 const R3 = () => {
+
+  const modalRef = useRef();
+  const playerRef = useRef();
+
+  const openModal = () => {
+    modalRef.current.style.display = "block";
+    const player = playerRef.current;
+    
+  
+  }
+
+  const closeModal = () => {
+    modalRef.current.style.display = "none";
+  }  
+
   return (
-    <div>
-      R3
+    <div className="video-wrapper">
+      <div className="video" onClick={ openModal }>
+        <div className="icon">
+          <div className="arrow"></div>
+        </div>
+        <img           
+          src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg" 
+          alt="Kitty video"
+        />
+      </div>
+      <div className="text">
+        <h3>Watch this video!</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Curabitur mattis lacus vitae nisi luctus sodales. 
+          Etiam euismod tristique nisl vel consequat. 
+          Donec ut risus non urna vestibulum bibendum. 
+          Pellentesque scelerisque, mauris a eleifend vehicula, 
+          magna ipsum egestas nunc, sit amet ultricies eros ante vel erat.
+          Aliquam ante elit, convallis vel risus eget, vehicula porta dui. 
+          Suspendisse hendrerit diam quis sem lobortis venenatis. 
+          In nec ultrices enim. 
+        </p> 
+      </div>
+      <div className="modal" onClick={ closeModal } ref={ modalRef } >
+        <div className="modal-content">
+          <iframe 
+            id="player"
+            ref={ playerRef }
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen            
+          >
+          </iframe>
+        </div>
+      </div> 
     </div>
   )
 }
